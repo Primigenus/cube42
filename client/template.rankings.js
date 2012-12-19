44e9972f5a1c217687f3f204c2d62b455b312b7a
Rahul Choudhury <rahul@q42.nl>
2012-12-19 21:40:00 +0100

    Update rankings display

--- client/template.rankings.js
+++ client/template.rankings.js
@@ -1,5 +1,15 @@
 
+Template.rankings.events({
+  "click [data-role='welcome']": function() {
+    $("#start").show();
+    $("#rankings").hide();
+  }
+})
 
 Template.rankings.users = function() {
   return Meteor.users.find({}, {sort: {maxLevelReached: -1}});
+}
+
+Template.rankings.isUser = function() {
+  return this.profile.name == Meteor.user().profile.name;
 }
