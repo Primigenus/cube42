4fb1fb64e28e4308de63c4cc8ea4d5dc0ad81386
Rahul Choudhury <rahul@q42.nl>
2012-12-20 00:14:10 +0100

    Refactor Move email method to server where it belongs, but don't call it since the db is easier

--- client/template.gifts.js
+++ client/template.gifts.js
@@ -11,12 +11,7 @@ Template.gifts.events({
     else {
       Gifts.update({}, {$pull: {recipients: Meteor.user().profile.name}}, {multi: true});
       Gifts.update(this._id, {$inc: {count: 1}, $push: {recipients: Meteor.user().profile.name}});
-      Email.send({
-        from: "rahul@q42.nl",
-        to: ["chris@q42.nl", "rahul@q42.nl"],
-        subject: "You picked a gift with Cube42!",
-        text: "This is a notification to let you know that " + Meteor.user().profile.name + " just picked '" + this.name + "'. Alright, excellent! Party on, Wayne!"
-      })
+      //Meteor.call("sendGiftEmail", Meteor.user().profile.name, this.name);
     }
     $(evt.target).toggleClass("selected");
   }
@@ -35,12 +30,14 @@ Template.giftItem.rendered = function() {
   var gift = Gifts.findOne({recipients: Meteor.user().profile.name});
   if (!gift) return;
 
+  var hails = ["Great", "Awesome", "Excellent", "Wicked", "Wizard", "Cool", "Dope", "Nice", "OK", "Fantastic", "Alright", "Thanks", "Dude"];
+  var hail = hails[~~(Math.random() * hails.length)];
+
   var name = gift.name;
   if (name[0] != "M") name[0].toLowerCase() + name.substring(1);
-  var hails = ["Great", "Awesome", "Excellent", "Wicked", "Wizard", "Cool", "Dope", "Nice", "OK", "Fantastic", "Alright", "Thanks", "Dude"];
   var type = "a";
   if (name.indexOf("2") > -1)
     type = "";
 
-  $("#gifts h2").text(hails[~~(Math.random() * hails.length)] + "! We'll send you " + type + " " + name);
+  $("#gifts h2").text(hail + "! We'll send you " + type + " " + name);
 }
